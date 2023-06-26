bd0a3166b2d017ae1f2a390970bf9b62cb45ee11
Andy Klimczak <andyklimczak@fastmail.com>
2023-06-26 23:43:45 +0100

    ch14

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -2,18 +2,42 @@ package main
 
 import (
   "bytes"
+  "html"
   "io"
   "log"
   "net/http"
   "net/http/cookiejar"
+  "net/url"
   "net/http/httptest"
+  "regexp"
   "testing"
+  "time"
+
+  "snippetbox.alexedwards.net/internal/models/mocks"
+
+  "github.com/alexedwards/scs/v2"
+  "github.com/go-playground/form/v4"
 )
 
 func newTestApplication(t *testing.T) *application {
+  templateCache, err := newTemplateCache()
+  if err != nil {
+    t.Fatal(err)
+  }
+  formDecoder := form.NewDecoder()
+
+  sessionManager := scs.New()
+  sessionManager.Lifetime = 12 * time.Hour
+  sessionManager.Cookie.Secure = true
+
   return &application{
     errorLog: log.New(io.Discard, "", 0),
     infoLog: log.New(io.Discard, "", 0),
+    snippets: &mocks.SnippetModel{},
+    users: &mocks.UserModel{},
+    templateCache: templateCache,
+    formDecoder: formDecoder,
+    sessionManager: sessionManager,
   }
 }
 
@@ -42,6 +66,36 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, strin
   if err != nil {
     t.Fatal(err)
   }
+  defer rs.Body.Close()
+  body, err := io.ReadAll(rs.Body)
+  if err != nil {
+    t.Fatal(err)
+  }
+  bytes.TrimSpace(body)
+  return rs.StatusCode, rs.Header, string(body)
+}
+
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body string) string {
+    // Use the FindStringSubmatch method to extract the token from the HTML body.
+    // Note that this returns an array with the entire matched pattern in the
+    // first position, and the values of any captured data in the subsequent
+    // positions.
+    matches := csrfTokenRX.FindStringSubmatch(body)
+    if len(matches) < 2 {
+        t.Fatal("no csrf token found in body")
+    }
+
+    return html.UnescapeString(string(matches[1]))
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+  rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+  if err != nil {
+    t.Fatal(err)
+  }
+
   defer rs.Body.Close()
   body, err := io.ReadAll(rs.Body)
   if err != nil {
